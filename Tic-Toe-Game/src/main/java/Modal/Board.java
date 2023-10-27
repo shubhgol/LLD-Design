e02c4a6b13850d4702568030c12f284596150747
Shubham kumar Madheshiya <shubham1205skm@gmail.com>
2023-10-27 16:16:00 +0530

    Update Board.java

--- Tic-Toe-Game/src/main/java/Modal/Board.java
+++ Tic-Toe-Game/src/main/java/Modal/Board.java
@@ -2,54 +2,47 @@ package Modal;
 
 public class Board {
     public int size;
-    public PlayingSymbol[][] board;
-
+    public PlayingSymbol [][]board;
     public Board(int size){
-        this.size = size;
-        board = new PlayingSymbol[size][size];
+         this.size = size;
+         this.board = new PlayingSymbol[size][size];
     }
 
     public void printBoard(){
-        int n = board.length;;
-        int m = board[0].length;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m;j++){
-                if(board[i][j]!=null){
-                    System.out.print(board[i][j].symbol.name());
-                }
-                System.out.print("  |");
-            }
-            System.out.println("");
-        }
-    }
-
-    public boolean isFreeSpaceExist(){
-        boolean isEmptyCellExist = false;
-
-        for(int i=0; i<board.length; i++){
-            if(isEmptyCellExist) break;
-            for(int j=0; j<board[0].length; j++){
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
                 if(board[i][j] == null){
-                    isEmptyCellExist = true;
-                    break;
+                    System.out.print("   ");
+                } else {
+                    System.out.print(" "+board[i][j].symbol+" ");
                 }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean addSymbol(int row, int column, PlayingSymbol symbol){
+        if(row<0 || row>=size || column<0 || column>=size || board[row][column]!=null){
+            return  false;
+        }
+        board[row][column] = symbol;
+        return  true;
+    }
+
+    public boolean checkFreeCellInBoard(){
+        boolean isFreeSpace = false;
+
+        for(int i=0; i<size; i++){
+            if(isFreeSpace) break;
+            for(int j = 0; j<size; j++){
+                  if(board[i][j]==null){
+                      isFreeSpace = true;
+                      break;
+                  }
             }
         }
 
-        return  isEmptyCellExist;
-    }
-
-    public boolean addSymbol(int row, int column, PlayingSymbol playingSymbol){
-        boolean isPlayerSymbolAddedSuccessfully = false;
-
-        if(board[row][column]!=null){
-             return  isPlayerSymbolAddedSuccessfully;
-        }
-
-        board[row][column] = playingSymbol;
-        isPlayerSymbolAddedSuccessfully = true;
-
-        return  isPlayerSymbolAddedSuccessfully;
-
+        return  isFreeSpace;
     }
 }
